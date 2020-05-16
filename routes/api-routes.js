b3af9e96efd48090f7e667879bd7956e49c3cc30
Philip Hurst <youngphil9@gmail.com>
2020-05-16 12:14:38 -0500

    added  to /api/workouts/range get route, created put route to find and update a workout

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -25,7 +25,39 @@ router.get("/api/workouts", (req, res) => {
 router.get("/api/workouts/range", (req, res) => {
   db.Workout.find(
     {
-      distance: {$not: null} 
+      distance: {$in: "workouts", $not: null} 
+    })
+    // .sort({distance: -1})
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  db.Workout.updateOne(
+    {
+      _id: req.params.id
+    },
+    {
+      workouts:
+      {
+        $in: req.body
+      }
+    },
+    {
+      $set:
+      {
+        name: req.body.name,
+        type: req.body.type,
+        weight: req.body.weight,
+        sets: req.body.sets,
+        reps: req.body.reps,
+        duration: req.body.duration,
+        distance: req.body.distance
+      }
     })
     .then(dbWorkouts => {
       res.json(dbWorkouts);
