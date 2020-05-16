596df97e2ec9cc1de8e3674efc26173ae06f2a41
Philip Hurst <youngphil9@gmail.com>
2020-05-16 12:43:55 -0500

    fixed mongoose connection for heroku deployment

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines");
 
 app.use(require("./routes/api-routes.js"));
 app.use(require("./routes/html-routes.js"));
